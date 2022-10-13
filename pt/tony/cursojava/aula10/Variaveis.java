102684d0d978a38ab57c979e116c72f0cd928909
tonyfilho <oficinadosdrones@gmail.com>
2022-10-13 14:01:41 +0100

    start with pakage

---
+++ pt/tony/cursojava/aula10/Variaveis.java
@@ -0,0 +1,24 @@
+package pt.tony.cursojava.aula10;
+
+public class Variaveis {
+    public static void main(String[] args) 
+    {
+        
+    }
+        
+    
+    /** Como se declara uma Var no Java
+* 1ª declara o Tipo e po Nome da Var :
+* 2º Convenção: Não pode usar palavras Reservada
+* 3º 1ª Letra a-z ou A-Z ou _
+* 4ª Case Sensitive
+* 5º 1º caracter usar LOWCASE e depois CamelCase  
+*/
+
+/** *******TIPOS Primitivos***************** */
+/*
+ * Int ou Inteiros
+   Flot ou Numeros com ponto ou virgula
+ */
+
+}  
