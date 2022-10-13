9f00319361f95641f8ec7af91f6dd46cf8b662c0
tonyfilho <oficinadosdrones@gmail.com>
2022-10-13 12:58:55 +0100

    class 05 and 7

---
+++ java-aula-05-a-06/Aula05HelloWord.java
@@ -0,0 +1,5 @@
+class Aula05HelloWord {
+    public static void main(String[] args) {
+        System.out.println("Hello ze");
+    }
+}
---
+++ java-aula-05-a-06/Aula06Argumentos.java
@@ -0,0 +1,7 @@
+public class Aula06Argumentos {
+  public static void main(String[] args) {
+        
+   System.out.printf("Voce digitou Args: ", args.length);
+        
+    }
+}
