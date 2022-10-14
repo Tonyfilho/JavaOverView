84d6c5f4e5e5e45d05aa8cef13a4e69fadbba0a7
tonyfilho <oficinadosdrones@gmail.com>
2022-10-14 17:11:07 +0100

    entity lifecycle callback

--- hello-todo/src/main/java/academy/learnprogramming/entity/Todo.java
+++ hello-todo/src/main/java/academy/learnprogramming/entity/Todo.java
@@ -4,10 +4,88 @@
  */
 package academy.learnprogramming.entity;
 
+import java.time.LocalDate;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.PrePersist;
+
 /**
  *
  * @author Tony
  */
+@Entity /*Informa ao JAVAX que este Objeto é uam ENTIDADE*/
 public class Todo {
+    
+    @Id    /*Informa ao JAVAX que isto é um ID*/
+    @GeneratedValue(strategy = GenerationType.AUTO)/*Gera automaticamente o Id na DB */
+    private Long id;
+    
+    private String task;
+    private boolean  isCompleted;
+    private LocalDate dateCompleted;
+    private LocalDate dueDate;
+    private LocalDate dateCreated;
+    
+    
+    /* Iniciando a data */
+    @PrePersist  /* este Life-Cycle transforma este Metodo em Entity Lifecycle Callback e ja vai para dentro da DB a informação setada*/
+    private void init() {
+        setDateCreated(LocalDate.now());
+    }
+    
+
+    
+    
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+    
+    public String getTask() {
+        return task;
+    }
+
+    public void setTask(String task) {
+        this.task = task;
+    }
+
+    public boolean isIsCompleted() {
+        return isCompleted;
+    }
+
+    public void setIsCompleted(boolean isCompleted) {
+        this.isCompleted = isCompleted;
+    }
+
+    public LocalDate getDateCompleted() {
+        return dateCompleted;
+    }
+
+    public void setDateCompleted(LocalDate dateCompleted) {
+        this.dateCompleted = dateCompleted;
+    }
+
+    public LocalDate getDueDate() {
+        return dueDate;
+    }
+
+    public void setDueDate(LocalDate dueDate) {
+        this.dueDate = dueDate;
+    }
+
+    public LocalDate getDateCreated() {
+        return dateCreated;
+    }
+
+    public void setDateCreated(LocalDate dateCreated) {
+        this.dateCreated = dateCreated;
+    }
+
+    
     
 }
